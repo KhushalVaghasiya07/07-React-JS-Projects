f6bf51b731adfc9bc3dd240f8abd3a8dcc522c3f
Khushal vaghasiya <khushalvaghasiya0@gmail.com>
2025-05-17 14:58:47 +0530

    Delete PR-7 Localbox Miner directory

--- PR-7 Localbox Miner/src/Services/service.js
+++
@@ -1,7 +0,0 @@
-export const setStorageData = (PatientData) => {
-  localStorage.setItem('PatientsInfo', JSON.stringify(PatientData));
-};
-
-export const getStorageData = () => {
-  return JSON.parse(localStorage.getItem('PatientsInfo')) || [];
-};
